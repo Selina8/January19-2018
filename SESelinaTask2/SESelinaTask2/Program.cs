cff659dff2378159dec4670c8f0fe4efbeab10b9
Elod Vizi <elod.vizi@yahoo.com>
2018-01-19 12:49:47 +0200

    The beginning

---
+++ SESelinaTask2/SESelinaTask2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SESelinaTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //So far you have only worked with main branch
+        }
+    }
+}
